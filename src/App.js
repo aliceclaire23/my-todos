f5ac733e4be1ba43935093d42c62ca04cfafe8c8
Alice-Claire <aliceclaireh@hotmail.co.uk>
2019-07-04 15:27:41 +0100

    delete and toggle working, filter buttons  WIP

--- src/App.js
+++ src/App.js
@@ -6,20 +6,22 @@ import './App.css';
 class App extends React.Component {
   state = {
     items: [
-      { text: 'eat', id: 1, completed: false },
-      { text: 'sleep', id: 2, completed: false },
-      { text: 'react', id: 3, completed: true }
+      { text: 'eat', id: 1, completed: false, category: 'lifestyle' },
+      { text: 'sleep', id: 2, completed: false, category: 'rest' },
+      { text: 'react', id: 3, completed: false, category: 'code' },
+      { text: 'node', id: 4, completed: false, category: 'code' }
     ]
   };
 
   render() {
     return (
-      <div className="App">
-        <Heading name="Sherpal" />
+      <div className='App'>
+        <Heading name='Sherpal' />
         <List
           items={this.state.items}
           removeItem={this.removeItem}
           toggleComplete={this.toggleComplete}
+          displayCategory={this.displayCategory}
         />
       </div>
     );
@@ -27,15 +29,35 @@ class App extends React.Component {
 
   toggleComplete = itemToToggle => {
     console.log('toggle this>', itemToToggle);
+    this.setState(state => {
+      return {
+        items: state.items.map(item => {
+          if (item === itemToToggle) {
+            if (item.completed) item.completed = false;
+            else item.completed = true;
+          }
+          return item;
+        })
+      };
+    });
   };
 
   removeItem = itemToRemove => {
-    console.log('removing', itemToRemove);
-    // this.setState(state => {
-    //   return {
-    //     items:
-    //   }
-    // })
+    console.log('removing', itemToRemove.text);
+    this.setState(state => {
+      return {
+        items: state.items.filter(item => item !== itemToRemove)
+      };
+    });
+  };
+
+  displayCategory = category => {
+    console.log('displaying category ', category);
+    this.setState(state => {
+      return {
+        items: state.items.filter(item => item.category === category)
+      };
+    });
   };
 }
 
--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
-const List = ({ items, removeItem }) => {
+const List = ({ items, removeItem, toggleComplete, displayCategory }) => {
   return (
-    <ul>
-      {items.map((item, i) => (
-        <li key={i}>
-          <span className={item.completed ? 'complete' : null}>
-            {item.text}
-          </span>
-          <button onClick={() => removeItem(item)}>Delete</button>
-        </li>
-      ))}
-    </ul>
+    <div>
+      <p>Item count: {items.length}</p>
+      <p>Completed items: {items.filter(item => item.completed).length}</p>
+      <ul>
+        {items.map((item, i) => (
+          <li key={i} onClick={() => toggleComplete(item)}>
+            <span className={item.completed ? 'complete' : null}>
+              {item.text}
+            </span>
+            <button onClick={() => removeItem(item)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => displayCategory('lifestyle')}>Lifestyle</button>
+      <button onClick={() => displayCategory('rest')}>Rest</button>
+      <button onClick={() => displayCategory('code')}>Code</button>
+    </div>
   );
 };
 
